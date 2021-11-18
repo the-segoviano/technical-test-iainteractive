a5a713fd42af4f32108dd6d38399b0cbe226dc03
Luis Segoviano <the.segoviano@gmail.com>
2021-11-17 22:36:12 -0600

    Configuracion de Git y configuracion de archivos iniciales del proyecto

--- IAInteractive/ViewController.swift
+++ IAInteractive/ViewController.swift
@@ -8,12 +8,23 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        self.view.backgroundColor = .white
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "Here is where everything start!"
+        label.textAlignment = .center
+        self.view.addSubview(label)
+        label.widthAnchor.constraint(equalToConstant: self.view.bounds.width).isActive = true
+        label.heightAnchor.constraint(equalToConstant: 30).isActive = true
+        label.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
+        label.centerYAnchor.constraint(equalTo: self.view.centerYAnchor).isActive = true
+        
     }
-
-
+    
+    
 }
 
